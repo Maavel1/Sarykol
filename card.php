bfcb9b6f46515807e498a3bd2cefe7e90577dada
yyyourdadyyy <repeteevkiril@gmail.com>
2023-05-29 13:42:22 +0600

    card done

--- card.php
+++ card.php
@@ -1,4 +1,24 @@
 <?php require 'header.php'; ?>
 <?php require 'sidebar.php'; ?>
-   
+<div class="card">
+    <div class="card-block">
+        <img src="" alt="">
+        <div class="card-title">
+
+        </div>
+        <div class="card-info">
+
+        </div>
+    </div>
+    <div class="card-block">
+        <div class="map">
+            <div class="m-s">
+        <script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A73943fae1944ad51612fcc58bf0897de8a2206ec81ac28b508c0319d625df107&amp;width=500&amp;height=400&amp;lang=ru_RU&amp;border-radius: 50px&amp;scroll=true">
+        </script>
+            </div>
+            <a href="https://yandex.kz/maps/-/CCUs6PHYxA" class="m-btn">Показать на карте</a>
+        </div>
+        <div class="ticket"><a href="" class="t-btn"></a></div>
+    </div>
+</div>   
 <?php require 'footer.php'; ?>

a2d627d38ac4a82e4e605fd08999979d24711cb0
yyyourdadyyy <repeteevkiril@gmail.com>
2023-05-29 14:33:06 +0600

    fix div

--- header.php
+++ header.php
@@ -28,32 +28,4 @@
             <img src="img/logo.png" alt="">
         </div></a>
 
-        <div class="menu">
-          <nav>
-            <div class="sidenav menu-sidenav">
-            <a class="home-link" href="index.php"><img class="img-home-sidebar" src="img/home-icon.png" alt="">Главная</a>
-              <a href="cities.php"><img src="img/city.png" alt=""> ГОРОДА</a>
-<<<<<<< HEAD
-              <a href="resorts.html"><img src="img/umbrella.png" alt=""> КУРОРТЫ</a>
-              <a href="monument.html"><img src="img/footprint.png" alt=""> ЗАПОВЕДНИКИ</a>
-              <a href="monument.html"><img src="img/monument.png" alt=""> ПАМЯТНИКИ</a>
-=======
-              <a href="cities.php"><img src="img/umbrella.png" alt=""> КУРОРТЫ</a>
-              <a href="cities.php"><img src="img/footprint.png" alt=""> ЗАПОВЕДНИКИ</a>
-              <a href="cities.php"><img src="img/monument.png" alt=""> ПАМЯТНИКИ</a>
->>>>>>> b534bf77ec0e0747c8dcb6ff673ea707af4b936e
-              </div>
-          </nav>
-        </div>
-        <div class="nav">
-            <div class="nav-item">
-               <div class="search">
-                <input type="text" class="serach-txt" placeholder="Поиск">
-                <a href="" class="search-btn"><img src="img/zoom.png" alt=""></a>
-               </div>
-            </div>
-            <div class="nav-item">
-                <a class="profile" href="profile.html"><img src="img/profile.png" alt=""></a> 
-             </div>
-        </div>
-    </div>
+</div>

8c8d5d8e18fe97918519c3b73003f14c2cc8d9dd
yyyourdadyyy <repeteevkiril@gmail.com>
2023-05-29 12:58:27 +0600

    full index php

---
+++ sidebar.php
@@ -0,0 +1,6 @@
+<div class="sidenav">
+    <a href="cities.html"><img src="img/city.png" alt=""> ГОРОДА</a>
+    <a href="resorts.html"><img src="img/umbrella.png" alt=""> КУРОРТЫ</a>
+    <a href="monument.html"><img src="img/footprint.png" alt=""> ЗАПОВЕДНИКИ</a>
+    <a href="monument.html"><img src="img/monument.png" alt=""> ПАМЯТНИКИ</a>
+  </div>
